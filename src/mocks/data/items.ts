8263375e51702b87ba396cd1c5261a78d35c33f1
Nathan <npieraut@live.fr>
2025-04-18 17:05:49 -0400

    new items

--- src/mocks/data/items.ts
+++ src/mocks/data/items.ts
@@ -3,5 +3,8 @@ import { Item } from '@/types/core';
 export const mockItems: Item[] = [
   { id: 1, name: 'Milk', image: '/milk.png' },
   { id: 2, name: 'Eggs', image: '/eggs.png' },
-  { id: 3, name: 'Cereal', image: '/cereal.png' }
+  { id: 3, name: 'Cereal', image: '/cereal.png' },
+  { id: 4, name: 'Spaghetti', image: '/spagetti.png' },
+  { id: 5, name: 'Ground Meat', image: '/ground-meat.png' },
+  { id: 6, name: 'Coffee', image: '/coffee.png' }
 ]; 
