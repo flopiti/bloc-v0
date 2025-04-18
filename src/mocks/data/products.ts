850d547be6075d58ec2f9ebb0701f300064987f2
Nathan <npieraut@live.fr>
2025-04-18 11:25:36 -0400

    base mock dat a

---
+++ src/mocks/data/products.ts
@@ -0,0 +1,7 @@
+import { Product } from '../../services/productService';
+
+export const mockProducts: Product[] = [
+  { id: 1, name: 'Milk', image: '/milk.png' },
+  { id: 2, name: 'Eggs', image: '/eggs.png' },
+  { id: 3, name: 'Cereal', image: '/cereal.png' }
+]; 
