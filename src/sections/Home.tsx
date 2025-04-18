65668fdd8d75702cdb3d8bb9c2a6fb365aa5cdf3
Nathan <npieraut@live.fr>
2025-04-18 12:36:52 -0400

    re-enter transition

--- src/sections/Home.tsx
+++ src/sections/Home.tsx
@@ -1,11 +1,15 @@
 import { DRAWER_DELAY } from "@/constants/animations";
 import { motion } from "framer-motion";
-
 const Home = () => {
   return (
     <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
       exit={{ opacity: 0 }}
-      transition={{ delay: DRAWER_DELAY}}
+      transition={{ 
+        delay: DRAWER_DELAY/2,
+        duration: 0.3
+      }}
     >
       <h1>Home</h1>
     </motion.div>
