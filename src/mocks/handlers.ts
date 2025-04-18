1c132be6517130ce3ac08fee35a66a7c0fb5c1de
Nathan <npieraut@live.fr>
2025-04-18 11:55:58 -0400

    get current cart

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -3,7 +3,16 @@ import { mockItems } from './data/items';
 
 export const handlers = [
   http.get(`${import.meta.env.VITE_API_BASE_URL}/items`, async () => {
-    await delay(150);
+    await delay(1000);
     return HttpResponse.json(mockItems);
+  }),
+  http.get(`${import.meta.env.VITE_API_BASE_URL}/cart`, async () => {
+    await delay(1000);
+    // Return just the first item (Milk) as the initial cart item
+    return HttpResponse.json(
+      mockItems
+        .sort(() => Math.random() - 0.5)
+        .slice(0, Math.floor(Math.random() * (mockItems.length + 1)))
+    );
   }),
 ]; 
