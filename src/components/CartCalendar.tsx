c307e06b5fa8c68c6050dc452f3aa888e56541fb
Nathan <npieraut@live.fr>
2025-04-22 13:02:43 -0400

    extract

--- src/components/CartCalendar.tsx
+++ src/components/CartCalendar.tsx
@@ -10,8 +10,28 @@ interface CartCalendarProps {
 }
 
 const CartCalendar = ({ cart }: CartCalendarProps) => {
-    const { setDeliveryDate } = useCart();
+
+
+    return (
+        <div className="flex flex-col p-4 border-2 border-secondary rounded-3xl my-2">
+
+            {cart?.nextDelivery ? (
+                <ActualCalendar cart={cart} />
+            ) : (
+                <div>
+                    <span>No next delivery date</span>
+                </div>
+            )}
+        </div>
+    )
+}
+
+export default CartCalendar;    
+
+
+const ActualCalendar = ({ cart }: CartCalendarProps) => {
     const [selectedDay, setSelectedDay] = useState<number | null>(null);
+    const { setDeliveryDate } = useCart();
     
     const today = useMemo(() => dayjs().day(), []);
     const weekDays = useMemo(() => {
@@ -48,65 +68,62 @@ const CartCalendar = ({ cart }: CartCalendarProps) => {
         const targetDate = today.add(daysUntilTarget, 'day').toDate();
         setDeliveryDate(targetDate);
     };
-
     return (
-        <div className="flex flex-col p-4 border-2 border-secondary rounded-3xl my-2">
-            <div className="flex justify-between items-center">
-                <h1 className="text-white">Next Delivery</h1>
-                <span className="text-white/80 text-sm">
-                    {cart?.nextDelivery && dayjs(cart.nextDelivery).format('ddd, MMM D')}
-                </span>
-            </div>
-            <div className="flex pt-6 px-2 pb-4 justify-between relative">
-                <AnimatePresence>
-                    {weekDays.map((day, index) => (
+        <>
+        <div className="flex justify-between items-center">
+        <h1 className="text-white">Next Delivery</h1>
+        <span className="text-white/80 text-sm">
+            {cart?.nextDelivery && dayjs(cart.nextDelivery).format('ddd, MMM D')}
+        </span>
+    </div>
+    <div className="flex pt-6 px-2 pb-4 justify-between relative">
+        <AnimatePresence>
+            {weekDays.map((day, index) => (
+                <motion.div
+                    key={index}
+                    layout
+                    className={`
+                        w-8 h-8 flex items-center justify-center
+                        border border-gray-200 rounded
+                        text-white relative
+                        ${index === today  && nextDeliveryDay !== index ? 'border-[0.25rem] border-white font-bold' : ''}
+                        ${isDayAvailable(index) ? 'bg-[#3399ff]/40 cursor-pointer hover:bg-[#3399ff]/60' : 'cursor-not-allowed'}
+                        ${nextDeliveryDay === index ? 'bg-[#3399ff]' : ''}
+                    `}
+                    style={{
+                        transformOrigin: "center center"
+                    }}
+                    animate={{
+                        scale: isThisWeek && index === nextDeliveryDay ? 1.5 : 1,
+                        margin: selectedDay === index ? "0 0.5rem" : "0 0.25rem"
+                    }}
+                    transition={{ 
+                        duration: 0.3,
+                        ease: "easeInOut",
+                        layout: { duration: 0.3 }
+                    }}
+                    onClick={() => handleDayClick(index)}
+                >
+                    {isThisWeek && index === nextDeliveryDay ? (
                         <motion.div
-                            key={index}
-                            layout
-                            className={`
-                                w-8 h-8 flex items-center justify-center
-                                border border-gray-200 rounded
-                                text-white relative
-                                ${index === today  && nextDeliveryDay !== index ? 'border-[0.25rem] border-white font-bold' : ''}
-                                ${isDayAvailable(index) ? 'bg-[#3399ff]/40 cursor-pointer hover:bg-[#3399ff]/60' : 'cursor-not-allowed'}
-                                ${nextDeliveryDay === index ? 'bg-[#3399ff]' : ''}
-                            `}
-                            style={{
-                                transformOrigin: "center center"
-                            }}
                             animate={{
-                                scale: isThisWeek && index === nextDeliveryDay ? 1.5 : 1,
-                                margin: selectedDay === index ? "0 0.5rem" : "0 0.25rem"
+                                y: [0.5, -0.5, 0.5],
                             }}
-                            transition={{ 
-                                duration: 0.3,
-                                ease: "easeInOut",
-                                layout: { duration: 0.3 }
+                            transition={{
+                                duration: 0.4,
+                                repeat: Infinity,
+                                ease: "easeInOut"
                             }}
-                            onClick={() => handleDayClick(index)}
                         >
-                            {isThisWeek && index === nextDeliveryDay ? (
-                                <motion.div
-                                    animate={{
-                                        y: [0.5, -0.5, 0.5],
-                                    }}
-                                    transition={{
-                                        duration: 0.4,
-                                        repeat: Infinity,
-                                        ease: "easeInOut"
-                                    }}
-                                >
-                                    <TbTruckDelivery className="w-6 h-6" strokeWidth={0.75} />
-                                </motion.div>
-                            ) : (
-                                day
-                            )}
+                            <TbTruckDelivery className="w-6 h-6" strokeWidth={0.75} />
                         </motion.div>
-                    ))}
-                </AnimatePresence>
-            </div>
-        </div>
+                    ) : (
+                        day
+                    )}
+                </motion.div>
+            ))}
+        </AnimatePresence>
+    </div>
+    </>
     )
 }
-
-export default CartCalendar;    
