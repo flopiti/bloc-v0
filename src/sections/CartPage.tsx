e11644f59972c2c6484232193cffaeef20e8172a
Nathan <npieraut@live.fr>
2025-04-22 21:58:29 -0400

    base

--- src/sections/CartPage.tsx
+++ src/sections/CartPage.tsx
@@ -1,8 +1,29 @@
-
+import { Item, Cart } from "@/types/core";
+import { motion } from "framer-motion";
+import ItemBox from "@/components/ItemBox";
+import { useCartStore } from "@/stores/cartStore";
 
 const CartPage = () => {
+
+    const { cart } = useCartStore();
+    const cartItems = cart ? [...cart.confirmedItems, ...cart.pendingItems] : [];
+
     return (
-        <div>CartPage</div>
+        <div className="container mx-auto ">
+            <div className="bg-white/5 rounded-xl p-6">
+                <div className="flex flex-row flex-wrap gap-4">
+                    {cartItems.map((item: Item) => (
+                        <motion.div
+                            key={item.id}
+                            layout
+                            className="w-[calc(33.333%-1rem)]"
+                        >
+                            <ItemBox item={item} />
+                        </motion.div>
+                    ))}
+                </div>
+            </div>
+        </div>
     )
 }
 
