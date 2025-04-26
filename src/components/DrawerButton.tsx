7b323c5c7d4f725357049376600df29e6ee595ff
Nathan <npieraut@live.fr>
2025-04-26 12:38:03 -0400

    unconfirmed

--- src/components/DrawerButton.tsx
+++ src/components/DrawerButton.tsx
@@ -1,5 +1,7 @@
 import { BUTTON_HEIGHT, DEFAULT_TRANSITION } from "@/constants/animations";
-import { motion } from "framer-motion"
+import useCart from "@/hooks/useCart";
+import { useCartStore } from "@/stores/cartStore";
+import { motion, AnimatePresence } from "framer-motion"
 import { GrBasket } from "react-icons/gr";
 
 
@@ -8,7 +10,12 @@ interface DrawerButtonProps {
     setIsDrawerOpen: (isDrawerOpen: boolean) => void;
 }
 
-export const DrawerButton = ({isDrawerOpen,setIsDrawerOpen,} : DrawerButtonProps) => (
+export const DrawerButton = ({isDrawerOpen,setIsDrawerOpen,} : DrawerButtonProps) => {
+
+  const {cart} = useCartStore();
+  const unconfirmedCount = cart?.pendingItems.length;
+
+  return (
     <motion.div
       className="full-screen w-1/2 mx-auto rounded-3xl z-20"
       initial={{ bottom: 0}}
@@ -21,8 +28,22 @@ export const DrawerButton = ({isDrawerOpen,setIsDrawerOpen,} : DrawerButtonProps
         className="w-full h-[3.25rem] bg-white/5 hover:bg-white/10 text-white opacity-65 rounded-xl relative z-10 transition-colors flex items-center justify-center gap-2"
         onClick={() => setIsDrawerOpen(!isDrawerOpen)}
       >
-      <GrBasket className="mx-2 w-6 h-6 text-secondary" />
-          {isDrawerOpen ? "Close" : "Shopping Cart"}
+        <AnimatePresence>
+          {unconfirmedCount && unconfirmedCount > 0 ? (
+            <motion.div
+              initial={{ y: 20, opacity: 0 }}
+              animate={{ y: 0, opacity: 1 }}
+              exit={{ y: 20, opacity: 0 }}
+              className="bg-secondary/50 rounded-full px-3 py-1 text-sm font-medium"
+            >
+              {unconfirmedCount}
+            </motion.div>
+          ) : (
+            <GrBasket className="mx-2 w-6 h-6 text-secondary" />
+          )}
+        </AnimatePresence>
+        {isDrawerOpen ? "Close" : "Shopping Cart"}
       </motion.button>
     </motion.div>
-)
+  )
+}
