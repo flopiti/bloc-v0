64cdc60361b5a5be8af2ae2c35420bab96db8b90
Nathan <npieraut@live.fr>
2025-04-27 00:46:28 -0400

    size

--- src/sections/ProductsPage.tsx
+++ src/sections/ProductsPage.tsx
@@ -4,23 +4,26 @@ import { useState } from 'react';
 import { useProductsStore } from '@/stores/itemsStore';
 import { Product } from '@/types/core';
 
+const PRODUCT_BOX_HEIGHT = 190;
+const PRODUCT_EXPANDED_HEIGHT = 60;
+
 const ProductsPage = () => {
     const [expandedIndex, setExpandedIndex] = useState<number | null>(null);
     const { products } = useProductsStore();
     const getRowNumber = (index: number) => Math.floor(index / 2);
 
     return (
-            <div className="grid grid-cols-2 gap-4 mb-25" style={{ gridAutoRows: '200px' }}>
-                {products.map((product : Product, index) => (
-                    <motion.div
-                        key={product.id}
+        <div className="grid grid-cols-2 gap-4 mb-25" style={{ gridAutoRows: `${PRODUCT_BOX_HEIGHT}px` }}>
+            {products.map((product : Product, index) => (
+                <motion.div
+                    key={product.id}
                         className="bg-white/5 p-4 rounded-lg shadow-md cursor-pointer"
                         initial={{ height: "200px" }}
                         animate={{
-                            height: expandedIndex === index ? "240px" : "200px",
-                            y: expandedIndex !== null && getRowNumber(index) > getRowNumber(expandedIndex) ? 40 : 0
+                            height: expandedIndex === index ? `${PRODUCT_BOX_HEIGHT + PRODUCT_EXPANDED_HEIGHT}px` : `${PRODUCT_BOX_HEIGHT}px`,
+                            y: expandedIndex !== null && getRowNumber(index) > getRowNumber(expandedIndex) ? PRODUCT_EXPANDED_HEIGHT : 0
                         }}
-                        exit={{ height: "200px" }}
+                        exit={{ height: `${PRODUCT_BOX_HEIGHT}px` }}
                         transition={{ 
                             duration: 0.3, 
                             ease: "easeInOut",
