ec3ba1c3462346872982bab6420c24b780441092
Nathan <npieraut@live.fr>
2025-04-28 14:16:26 -0400

    extract skeleton

--- src/components/ProductBox.tsx
+++ src/components/ProductBox.tsx
@@ -6,6 +6,7 @@ import { useCartStore } from "@/stores/cartStore";
 import { FiCheck, FiChevronRight, FiChevronLeft } from "react-icons/fi";
 import QuantityInput from "./QuantityInput";
 import LeftRightNavigator from "./LeftRightNavigator";
+import ProductBoxSkeleton from "./ProductBoxSkeleton";
 
 interface ProductProps {
     isOpen: boolean;
@@ -75,18 +76,7 @@ const ProductBox = ({ isOpen, product, isLoading = false }: ProductProps) => {
     };
 
     if (isLoading) {
-        return (
-            <div className="relative h-full">
-                <div className="absolute top-0 left-0 right-0">
-                    <div className="h-[8rem] relative">
-                        <div className="h-full w-full bg-white/10 animate-pulse rounded-lg" />
-                    </div>
-                    <div className="h-[40px] mt-2">
-                        <div className="h-4 bg-white/10 rounded w-3/5 animate-pulse" />
-                    </div>
-                </div>
-            </div>
-        );
+        return <ProductBoxSkeleton />;
     }
 
     return (
