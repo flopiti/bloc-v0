3914fb029412d7ee6fe302ffdb9f4517232d1d22
Nathan <npieraut@live.fr>
2025-04-29 08:12:15 -0400

    add

--- src/sections/CartPage.tsx
+++ src/sections/CartPage.tsx
@@ -54,6 +54,17 @@ const CartPage = ({ goToPage }: CartPageProps) => {
                     />  
                 ) : (
                     <div className={`${isGridView ? 'flex flex-row flex-wrap' : 'flex flex-col'} gap-4 p-4`}>
+                        <motion.div
+                            layout
+                            className={isGridView ? "w-[calc(33.333%-1rem)]" : "w-full"}
+                        >
+                            <button
+                                onClick={() => goToPage(PAGE.PRODUCTS)}
+                                className={`rounded-lg overflow-hidden relative group cursor-pointer ${isGridView ? 'aspect-square' : 'h-24'} w-full border-2 border-dashed border-gray-400 bg-white/5 hover:bg-white/10 transition-colors flex items-center justify-center`}
+                            >
+                                <IoAdd className="text-gray-400 text-5xl" />
+                            </button>
+                        </motion.div>
                         {cartItems.map((item: Item) => (
                             <motion.div
                                 key={item.product.id}
@@ -79,17 +90,6 @@ const CartPage = ({ goToPage }: CartPageProps) => {
                                 </div>
                             </motion.div>
                         ))}
-                        <motion.div
-                            layout
-                            className={isGridView ? "w-[calc(33.333%-1rem)]" : "w-full"}
-                        >
-                            <button
-                                onClick={() => goToPage(PAGE.PRODUCTS)}
-                                className={`rounded-lg overflow-hidden relative group cursor-pointer ${isGridView ? 'aspect-square' : 'h-24'} w-full border-2 border-dashed border-gray-400 bg-white/5 hover:bg-white/10 transition-colors flex items-center justify-center`}
-                            >
-                                <IoAdd className="text-gray-400 text-5xl" />
-                            </button>
-                        </motion.div>
                     </div>
                 )}
             </div>
