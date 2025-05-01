6f74647a9d523a57988c0cc5e4b94a6e8ced6375
Nathan <npieraut@live.fr>
2025-05-01 14:30:40 -0400

    move some liones

--- src/components/Featured.tsx
+++ src/components/Featured.tsx
@@ -15,28 +15,27 @@ const Featured = () => {
         <div>
             <SubHeader text="Featured" />
             <PulseContainer>
-
-            {/* Image container */}
-            <div className="relative aspect-[16/9] ">
-                <img 
-                    src={'/featured.png'} 
-                    alt="Featured"
-                    className="w-full h-full object-cover "
-                />
-                
-                {/* Centered Add to cart button */}
-                <div className="absolute inset-0 flex items-end justify-center">
-                    <motion.button
-                        onClick={handleAddToCart}
-                        className="btn-primary px-6 mb-10 py-2 rounded-xl opacity-90"
-                        whileHover={{ scale: 1.05 }}
-                        whileTap={{ scale: 0.95 }}
-                    >
-                        Add to cart
-                    </motion.button>
+                {/* Image container */}
+                <div className="relative aspect-[16/9] ">
+                    <img 
+                        src={'/featured.png'} 
+                        alt="Featured"
+                        className="w-full h-full object-cover "
+                    />
+                    
+                    {/* Centered Add to cart button */}
+                    <div className="absolute inset-0 flex items-end justify-center">
+                        <motion.button
+                            onClick={handleAddToCart}
+                            className="btn-primary px-6 mb-10 py-2 rounded-xl opacity-90"
+                            whileHover={{ scale: 1.05 }}
+                            whileTap={{ scale: 0.95 }}
+                        >
+                            Add to cart
+                        </motion.button>
+                    </div>
                 </div>
-            </div>
-        </PulseContainer>
+            </PulseContainer>
         </div>
     );
 };
